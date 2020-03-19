a9c69e6d14e83e5b29fb6dba332dbce5b2a93c38
Ali Kamal <alikamaltaki@gmail.com>
2020-03-19 13:33:31 +0300

    store test 2

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Product;
 
 class ProductController extends Controller
 {
-    public function store()
+    public function store(Request $request)
     {
-        return response()->json([], 201);
+        $product = Product::create([
+            'name' => $request->name,
+            'slug' => str_slug($request->name),
+            'price' => $request->price
+        ]);
+
+        return response()->json($product, 201);
     }
 }
--- tests/Feature/Http/Controllers/Api/ProductControllerTest.php
+++ tests/Feature/Http/Controllers/Api/ProductControllerTest.php
@@ -2,9 +2,10 @@
 
 namespace Tests\Feature\Http\Controllers\Api;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Faker\Factory;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ProductControllerTest extends TestCase
 {
@@ -14,12 +15,26 @@ class ProductControllerTest extends TestCase
         // Given
 
         // When
-        $repsonse = $this->json('POST', '/api/products', [
+        $faker = Factory::create();
 
+        $repsonse = $this->json('POST', '/api/products', [
+            'name' => $name = $faker->company,
+            'slug' => str_slug($name),
+            'price' => $price = random_int(10, 100)
         ]);
 
         // Then
-        $repsonse->assertStatus(201);
-
+        $repsonse->assertStatus(201)
+            ->assertJsonStructure(['id', 'name', 'slug', 'price', 'created_at'])
+            ->assertJson([
+                'name' => $name,
+                'slug' => str_slug($name),
+                'price' => $price
+            ]);
+        $this->assertDatabaseHas('products', [
+            'name' => $name,
+            'slug' => str_slug($name),
+            'price' => $price
+        ]);
     }
 }
